88a78619950d9dfbd5d8dcbcbc82e41bc005a5df
Masa222a <66303621+Masa222a@users.noreply.github.com>
2022-01-08 18:20:16 +0900

    Update MainActivity.kt
    不要コメントの削除、onCreate内にbindingを変数として定義

--- app/src/main/java/android/tanmen/MainActivity.kt
+++ app/src/main/java/android/tanmen/MainActivity.kt
@@ -13,10 +13,10 @@ import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        lateinit var binding: ActivityMainBinding
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -24,8 +24,7 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_shuffle
