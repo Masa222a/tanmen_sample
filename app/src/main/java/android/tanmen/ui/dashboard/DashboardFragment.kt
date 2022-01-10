8ba06e1f572a01b1e2ed5a6c0e1397f5c9db07f9
Masa222a <66303621+Masa222a@users.noreply.github.com>
2022-01-10 20:59:23 +0900

    Update DashboardFragment.kt
    不要なコメントの削除、onCreateView内にbindingを定義

--- app/src/main/java/android/tanmen/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/android/tanmen/ui/dashboard/DashboardFragment.kt
@@ -10,36 +10,24 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import android.tanmen.R
 import android.tanmen.databinding.FragmentDashboardBinding
+import androidx.fragment.app.viewModels
 
 class DashboardFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
-    private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private val dashboardViewModel: DashboardViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val binding = FragmentDashboardBinding.inflate(inflater, container, false)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+            binding.textDashboard.text = it
         })
         return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
